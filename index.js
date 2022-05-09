434d57e59b4c3b4bfef6d77b9b04f16942dc20f8
RiadArefin <arefinriad40@gmail.com>
2022-05-09 18:30:29 +0600

    my item with mail

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ function verifuJWT(req,res,next){
 
 
 
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.foixi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
